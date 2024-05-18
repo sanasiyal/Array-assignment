eb4d0391e9e9f2f456e83ad859c1ba4280f4c321
sanasiyal <160250355+sanasiyal@users.noreply.github.com>
2024-05-18 09:42:16 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//Question 1
+var fruits = ["apple", "banana", "mango", "orange"];
+console.log(fruits);
+// //Question 2
+// let numbers:number[] = [10 ,20 ,30 ,40]
+// //Question 3
+// let thirdFruit = fruits[2]
+// // console.log(thirdFruit);
+// // Question 4
+// numbers[1] = 25
+// console.log(numbers);
+// //Question 5
+// fruits.push("grapes")
+// // console.log(fruits);
+// //Question 6
+// fruits.pop()
+// let lastFruit = fruits
+// // console.log(lastFruit);
+// //Question 7
+// // fruits.shift()
+// // let firstFruit = fruits
+// // console.log(firstFruit);
+// //Question 8
+// // fruits.unshift("kiwi")
+// // console.log(fruits);
+// //Question 9
+// fruits.splice(1 ,2)
+// console.log(fruits);
+// //Question 10
+// fruits.splice(2 , 0 , "pineapple" , "pear")
+// console.log(fruits);
+//Question 11
+// let citrusFruits:string[] = fruits.slice(0 ,2)
+// console.log(citrusFruits);
+// Question 12
+var lastTwoFruits = fruits.slice(2, 4);
+console.log(lastTwoFruits);
